da6875ddcda8eef79191dd481bd4afb63273333b
arzutepe <38106558+arzutp@users.noreply.github.com>
2019-05-09 16:48:59 +0300

    Rename main.c to pointer_2.c

--- pointer_2.c
+++ pointer_2.c
@@ -6,7 +6,7 @@ int main()
     int i,j=1;
     int *jp1,*jp2=&j;
 
-    jp1 = jp2;  ///pointera pointer atadik ikiside ayni adresi gösteriyor.
+    jp1 = jp2;  ///pointera pointer atadik ikiside ayni adresi gÃ¶steriyor.
 
     i = ++(*jp2); ///jp2 nin gosterdigi adresteki degeri arttirdik i ye atadik
     *jp2 = *jp1+i;  ///jp2 nin adresindeki degeri jp1 adresindeki deger ve i ile toplayip degistirdik
